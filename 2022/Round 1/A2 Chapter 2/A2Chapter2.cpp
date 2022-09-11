805149ab04d05675e2762ccd879631fa79737581
Nividh S <67477539+BeastBreath@users.noreply.github.com>
2022-09-10 18:04:52 -0700

    Started A2 Chapter 2

---
+++ 2022/Round 1/A2 Chapter 2/A2Chapter2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int T, N, K;
+
+
+
+
+int main() {
+    freopen("A2Chapter2in.txt", "r", stdin);
+    //freopen("A2Chapter2out.txt", "w", stdout);
+    
+    string str;
+
+    getline(cin, str);
+
+    cout << str << endl;
+}
